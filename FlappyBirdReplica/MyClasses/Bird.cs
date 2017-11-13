f8481aaea1590e4e49bab264d8d87f1b6b5de3c9
kyriuch <kyriuch@gmail.com>
2017-11-13 23:51:26 +0100

    Changed the whole updating system.

---
+++ FlappyBirdReplica/MyClasses/Bird.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace FlappyBirdReplica.MyClasses
+{
+	class Bird : Entity
+	{
+		public float JumpForce { get; set; }
+
+		public override void Update()
+		{
+			if(Keyboard.GetState().IsKeyDown(Keys.Space))
+			{
+				Velocity = new Vector2(0, JumpForce);
+			}
+		}
+	}
+}
